81174043f71b9d41727342d996cdca31d44fe83f
lmx <limingxu1985@gmail.com>
2022-01-11 16:59:47 +0800

    feature:(components) add new components Notes and NewNote

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,14 @@
 import { createNote, toggleImportanceOf } from './reducer/noteReducer'
 import { useSelector, useDispatch } from 'react-redux'
+import Notes from './Notes'
+import NewNote from './NewNote'
 
 function App() {
-    const dispatch = useDispatch()
-    const notes = useSelector((state) => state)
-
-    const addNote = (event) => {
-        event.preventDefault()
-        const content = event.target.note.value
-        event.target.note.value = ''
-        dispatch(createNote(content))
-        console.log(content)
-    }
-    const toggleImportance = (id) => {
-        dispatch(toggleImportanceOf(id))
-    }
-
     return (
         <>
-            <h1>React App</h1>
-            <form onSubmit={addNote}>
-                <input name="note" />
-                <button type="submit">add</button>
-            </form>
-            <ul>
-                {notes.map((note) => (
-                    <li key={note.id} onClick={() => toggleImportance(note.id)}>
-                        {note.content}
-                        <strong>
-                            {note.important ? 'important' : 'trivial'}
-                        </strong>
-                    </li>
-                ))}
-            </ul>
+            <h1>Redux App</h1>
+            <NewNote />
+            <Notes />
         </>
     )
 }
---
+++ src/NewNote.jsx
@@ -0,0 +1,23 @@
+import { useDispatch } from 'react-redux'
+import { createNote } from './reducer/noteReducer'
+
+const NewNote = (props) => {
+    const dispatch = useDispatch()
+
+    const addNote = (event) => {
+        event.preventDefault()
+        const content = event.target.note.value
+        event.target.note.value = ''
+        dispatch(createNote(content))
+        console.log(content)
+    }
+
+    return (
+        <form onSubmit={addNote}>
+            <input name="note" />
+            <button type="submit">add</button>
+        </form>
+    )
+}
+
+export default NewNote
